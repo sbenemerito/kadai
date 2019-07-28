0e63fe095714fdec9d03f0c83bac10b45130eed3
Sam Benemerito <benemeritosam@gmail.com>
2019-07-28 13:22:43 +0900

    use separate index file

--- tests.py
+++ tests.py
@@ -5,6 +5,8 @@ from core.constants import INDEX_FILE_DIR
 from core.indexer import generate_index_file
 from core.searcher import query_data
 
+TEST_INDEX_FILE = os.path.join(os.path.dirname(__file__), 'test_index.pickle')
+
 
 class TestApp(unittest.TestCase):
     """
@@ -13,10 +15,10 @@ class TestApp(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        generate_index_file()
+        generate_index_file(index_dir=TEST_INDEX_FILE)
 
     def test_generate_index(self):
-        index_file_exists = os.path.isfile(INDEX_FILE_DIR)
+        index_file_exists = os.path.isfile(TEST_INDEX_FILE)
         self.assertTrue(index_file_exists, "Should create index file")
 
     def test_search_shibuya(self):
@@ -27,7 +29,11 @@ class TestApp(unittest.TestCase):
         special_value = ('常盤町（東大路通渋谷上る、渋谷通東大路東入、'
                          '渋谷通東大路東入２丁目、東大路五条下る）')
 
-        query_result = query_data(search_term=search_term)
+        query_result = query_data(
+            search_term=search_term,
+            index_dir=TEST_INDEX_FILE
+        )
+
         for bigram_matches in query_result:
             for row in bigram_matches:
                 if row[0] in sample_outputs:
@@ -44,6 +50,11 @@ class TestApp(unittest.TestCase):
         with self.subTest():
             self.assertEqual(match_counter, 3,
                              'Should match all three sample outputs')
+
+    @classmethod
+    def tearDownClass(cls):
+        if os.path.isfile(TEST_INDEX_FILE):
+            os.remove(TEST_INDEX_FILE)
 
 
 if __name__ == '__main__':
